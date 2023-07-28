d0c5f4e7da24f401d6046790d8995cae47cbac0a
Leonardo <fernandes.leo@live.com>
2023-07-27 22:02:58 -0300

    Initial Commit

---
+++ TCG/TCGApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCGApp.swift
+//  TCG
+//
+//  Created by Leonardo Fernandes on 27/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TCGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
